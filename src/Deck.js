d17e08d0b7d73a10ede2fd7f11e96c81d70a701c
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-11 14:05:13 +0100

    created a getCard method which for now is console logging the output

--- src/Deck.js
+++ src/Deck.js
@@ -12,15 +12,22 @@ class Deck extends Component {
   }
 
   async componentDidMount() {
-    let deck = await axios.get(API_URL)
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`)
     this.setState({ deck: deck.data })
+  }
+
+  async getCard() {
+    let id = this.state.deck.deck_id
+    let cardUrl = `${API_BASE_URL}/${id}/draw/`
+    let cardRes = await axios.get(cardUrl)
+    console.log(cardRes)
   }
 
   render() {
     return (
       <div>
         <h1>Card dealer</h1>
-        <button onCLick={this.getCard}>Get card!</button>
+        <button onClick={this.getCard}>Get card!</button>
       </div>
     )
   }
